199dfb18c8b09f5caa4c2f0d4459dd3cb37a4d10
Florian Wetzels <wetzels@cs.uni-kl.de>
2025-08-09 00:52:22 +0200

    finished scripts

---
+++ scripts/compute_barycenters_four_clusters.py
@@ -0,0 +1,226 @@
+# state file generated using paraview version 5.10.1
+
+# uncomment the following three lines to ensure this script works in future versions
+#import paraview
+#paraview.compatibility.major = 5
+#paraview.compatibility.minor = 10
+
+#### import the simple module from the paraview
+from paraview.simple import *
+#### disable automatic camera reset on 'Show'
+paraview.simple._DisableFirstRenderCameraReset()
+
+# ----------------------------------------------------------------
+# setup the data processing pipelines
+# ----------------------------------------------------------------
+
+# create a new 'TTK CinemaReader'
+tTKCinemaReader1 = TTKCinemaReader(registrationName='TTKCinemaReader1', DatabasePath='/home/wetzels/data/vertical_swap_four_clusters.cdb')
+
+# create a new 'TTK CinemaProductReader'
+tTKCinemaProductReader1 = TTKCinemaProductReader(registrationName='TTKCinemaProductReader1', Input=tTKCinemaReader1)
+
+# create a new 'TTK MergeTree'
+tTKMergeandContourTreeFTM1 = TTKMergeTree(registrationName='TTKMergeandContourTreeFTM1', Input=tTKCinemaProductReader1)
+tTKMergeandContourTreeFTM1.ScalarField = ['POINTS', 'scalar']
+tTKMergeandContourTreeFTM1.InputOffsetField = ['POINTS', 'scalar']
+tTKMergeandContourTreeFTM1.TreeType = 'Split Tree'
+tTKMergeandContourTreeFTM1.UseAllCores = 0
+
+# find source
+tTKMergeandContourTreeFTM1_1 = FindSource('TTKMergeandContourTreeFTM1')
+
+# find source
+tTKMergeandContourTreeFTM1_2 = FindSource('TTKMergeandContourTreeFTM1')
+
+# create a new 'TTK BlockAggregator'
+tTKBlockAggregator1 = TTKBlockAggregator(registrationName='TTKBlockAggregator1', Input=[tTKMergeandContourTreeFTM1, OutputPort(tTKMergeandContourTreeFTM1_1,1), OutputPort(tTKMergeandContourTreeFTM1_2,2)])
+tTKBlockAggregator1.FlattenInput = 0
+
+# create a new 'TTK MergeTreeClustering'
+tTKMergeTreeClustering1 = TTKMergeTreeClustering(registrationName='TTKMergeTreeClustering1', Input=tTKBlockAggregator1,
+    OptionalInputclustering=None)
+tTKMergeTreeClustering1.ComputeBarycenter = 1
+tTKMergeTreeClustering1.Deterministic = 1
+tTKMergeTreeClustering1.DimensionSpacing = 0.002
+tTKMergeTreeClustering1.Epsilon1 = 0.0
+tTKMergeTreeClustering1.Epsilon2 = 100.0
+tTKMergeTreeClustering1.Epsilon3 = 100.0
+tTKMergeTreeClustering1.ImportantPairs = 31.0
+tTKMergeTreeClustering1.ImportantPairsSpacing = 0.2
+tTKMergeTreeClustering1.NonImportantPairsSpacing = 8.0
+tTKMergeTreeClustering1.UseAllCores = 0
+
+#============================================================================
+# rendering
+#============================================================================
+
+# get the material library
+materialLibrary1 = GetMaterialLibrary()
+
+# Create a new 'Render View'
+renderView1 = CreateView('RenderView')
+renderView1.ViewSize = [1503, 793]
+renderView1.InteractionMode = '2D'
+renderView1.AxesGrid = 'GridAxes3DActor'
+renderView1.CenterOfRotation = [69.47561645507812, 78.22714233398438, 0.0]
+renderView1.StereoType = 'Crystal Eyes'
+renderView1.CameraPosition = [69.47561645507812, 78.22714233398438, 3391.536147647599]
+renderView1.CameraFocalPoint = [69.47561645507812, 78.22714233398438, 0.0]
+renderView1.CameraFocalDisk = 1.0
+renderView1.CameraParallelScale = 725.4497084006886
+renderView1.BackEnd = 'OSPRay raycaster'
+renderView1.OSPRayMaterialLibrary = materialLibrary1
+
+SetActiveView(None)
+
+# ----------------------------------------------------------------
+# setup view layouts
+# ----------------------------------------------------------------
+
+# create new layout object 'Layout #1'
+layout1 = CreateLayout(name='Layout #1')
+layout1.AssignView(0, renderView1)
+layout1.SetSize(1503, 793)
+
+# ----------------------------------------------------------------
+# setup the visualization in view 'renderView1'
+# ----------------------------------------------------------------
+
+# show data from tTKMergeTreeClustering1
+tTKMergeTreeClustering1Display = Show(tTKMergeTreeClustering1, renderView1, 'UnstructuredGridRepresentation')
+
+# get color transfer function/color map for 'Scalar'
+scalarLUT = GetColorTransferFunction('Scalar')
+scalarLUT.RGBPoints = [0.028, 0.231373, 0.298039, 0.752941, 0.52, 0.865003, 0.865003, 0.865003, 1.027, 0.705882, 0.0156863, 0.14902]
+scalarLUT.ScalarRangeInitialized = 1.0
+
+# get opacity transfer function/opacity map for 'Scalar'
+scalarPWF = GetOpacityTransferFunction('Scalar')
+scalarPWF.Points = [0.028, 0.0, 0.5, 0.0, 1.02, 1.0, 0.5, 0.0]
+scalarPWF.ScalarRangeInitialized = 1
+
+# trace defaults for the display properties.
+tTKMergeTreeClustering1Display.Representation = 'Surface'
+tTKMergeTreeClustering1Display.ColorArrayName = ['POINTS', 'Scalar']
+tTKMergeTreeClustering1Display.LookupTable = scalarLUT
+tTKMergeTreeClustering1Display.LineWidth = 4.0
+tTKMergeTreeClustering1Display.SelectTCoordArray = 'None'
+tTKMergeTreeClustering1Display.SelectNormalArray = 'None'
+tTKMergeTreeClustering1Display.SelectTangentArray = 'None'
+tTKMergeTreeClustering1Display.OSPRayScaleArray = 'BranchNodeID'
+tTKMergeTreeClustering1Display.OSPRayScaleFunction = 'PiecewiseFunction'
+tTKMergeTreeClustering1Display.SelectOrientationVectors = 'None'
+tTKMergeTreeClustering1Display.ScaleFactor = 359.8905395507813
+tTKMergeTreeClustering1Display.SelectScaleArray = 'BranchNodeID'
+tTKMergeTreeClustering1Display.GlyphType = 'Arrow'
+tTKMergeTreeClustering1Display.GlyphTableIndexArray = 'BranchNodeID'
+tTKMergeTreeClustering1Display.GaussianRadius = 17.994526977539063
+tTKMergeTreeClustering1Display.SetScaleArray = ['POINTS', 'BranchNodeID']
+tTKMergeTreeClustering1Display.ScaleTransferFunction = 'PiecewiseFunction'
+tTKMergeTreeClustering1Display.OpacityArray = ['POINTS', 'BranchNodeID']
+tTKMergeTreeClustering1Display.OpacityTransferFunction = 'PiecewiseFunction'
+tTKMergeTreeClustering1Display.DataAxesGrid = 'GridAxesRepresentation'
+tTKMergeTreeClustering1Display.PolarAxes = 'PolarAxesRepresentation'
+tTKMergeTreeClustering1Display.ScalarOpacityFunction = scalarPWF
+tTKMergeTreeClustering1Display.ScalarOpacityUnitDistance = 941.4809873412064
+tTKMergeTreeClustering1Display.OpacityArrayName = ['POINTS', 'BranchNodeID']
+
+# init the 'PiecewiseFunction' selected for 'ScaleTransferFunction'
+tTKMergeTreeClustering1Display.ScaleTransferFunction.Points = [0.0, 0.0, 0.5, 0.0, 9.0, 1.0, 0.5, 0.0]
+
+# init the 'PiecewiseFunction' selected for 'OpacityTransferFunction'
+tTKMergeTreeClustering1Display.OpacityTransferFunction.Points = [0.0, 0.0, 0.5, 0.0, 9.0, 1.0, 0.5, 0.0]
+
+# find source
+tTKMergeTreeClustering1_1 = FindSource('TTKMergeTreeClustering1')
+
+# show data from tTKMergeTreeClustering1_1
+tTKMergeTreeClustering1_1Display = Show(OutputPort(tTKMergeTreeClustering1_1, 1), renderView1, 'UnstructuredGridRepresentation')
+
+# trace defaults for the display properties.
+tTKMergeTreeClustering1_1Display.Representation = 'Surface'
+tTKMergeTreeClustering1_1Display.ColorArrayName = ['POINTS', 'Scalar']
+tTKMergeTreeClustering1_1Display.LookupTable = scalarLUT
+tTKMergeTreeClustering1_1Display.LineWidth = 4.0
+tTKMergeTreeClustering1_1Display.SelectTCoordArray = 'None'
+tTKMergeTreeClustering1_1Display.SelectNormalArray = 'None'
+tTKMergeTreeClustering1_1Display.SelectTangentArray = 'None'
+tTKMergeTreeClustering1_1Display.OSPRayScaleArray = 'BranchNodeID'
+tTKMergeTreeClustering1_1Display.OSPRayScaleFunction = 'PiecewiseFunction'
+tTKMergeTreeClustering1_1Display.SelectOrientationVectors = 'None'
+tTKMergeTreeClustering1_1Display.ScaleFactor = 14.778521537780762
+tTKMergeTreeClustering1_1Display.SelectScaleArray = 'BranchNodeID'
+tTKMergeTreeClustering1_1Display.GlyphType = 'Arrow'
+tTKMergeTreeClustering1_1Display.GlyphTableIndexArray = 'BranchNodeID'
+tTKMergeTreeClustering1_1Display.GaussianRadius = 0.7389260768890381
+tTKMergeTreeClustering1_1Display.SetScaleArray = ['POINTS', 'BranchNodeID']
+tTKMergeTreeClustering1_1Display.ScaleTransferFunction = 'PiecewiseFunction'
+tTKMergeTreeClustering1_1Display.OpacityArray = ['POINTS', 'BranchNodeID']
+tTKMergeTreeClustering1_1Display.OpacityTransferFunction = 'PiecewiseFunction'
+tTKMergeTreeClustering1_1Display.DataAxesGrid = 'GridAxesRepresentation'
+tTKMergeTreeClustering1_1Display.PolarAxes = 'PolarAxesRepresentation'
+tTKMergeTreeClustering1_1Display.ScalarOpacityFunction = scalarPWF
+tTKMergeTreeClustering1_1Display.ScalarOpacityUnitDistance = 37.419744034651195
+tTKMergeTreeClustering1_1Display.OpacityArrayName = ['POINTS', 'BranchNodeID']
+
+# init the 'PiecewiseFunction' selected for 'ScaleTransferFunction'
+tTKMergeTreeClustering1_1Display.ScaleTransferFunction.Points = [0.0, 0.0, 0.5, 0.0, 25.0, 1.0, 0.5, 0.0]
+
+# init the 'PiecewiseFunction' selected for 'OpacityTransferFunction'
+tTKMergeTreeClustering1_1Display.OpacityTransferFunction.Points = [0.0, 0.0, 0.5, 0.0, 25.0, 1.0, 0.5, 0.0]
+
+# setup the color legend parameters for each legend in this view
+
+# get color legend/bar for scalarLUT in view renderView1
+scalarLUTColorBar = GetScalarBar(scalarLUT, renderView1)
+scalarLUTColorBar.Title = 'Scalar'
+scalarLUTColorBar.ComponentTitle = ''
+
+# set color bar visibility
+scalarLUTColorBar.Visibility = 1
+
+# show color legend
+tTKMergeTreeClustering1Display.SetScalarBarVisibility(renderView1, True)
+
+# show color legend
+tTKMergeTreeClustering1_1Display.SetScalarBarVisibility(renderView1, True)
+
+# ----------------------------------------------------------------
+# restore active view
+SetActiveView(renderView1)
+# ----------------------------------------------------------------
+
+# get active view
+renderView1 = GetActiveViewOrCreate('RenderView')
+
+# reset view to fit data
+renderView1.ResetCamera(False)
+
+# get layout
+layout1 = GetLayout()
+
+# layout/tab size in pixels
+layout1.SetSize(1503, 793)
+
+# current camera placement for renderView1
+renderView1.InteractionMode = '2D'
+renderView1.CameraPosition = [69.47561645507812, 78.22714233398438, 3391.536147647599]
+renderView1.CameraFocalPoint = [69.47561645507812, 78.22714233398438, 0.0]
+renderView1.CameraParallelScale = 725.4497084006886
+
+# save screenshot
+SaveScreenshot('/home/wetzels/barycenter_four_clusters_wsd.png', renderView1, ImageResolution=[3006, 1586],
+    TransparentBackground=0)
+    
+    
+tTKMergeTreeClustering1.Backend = 'Path Mapping Distance (TopoInVis 2022)'
+
+# save screenshot
+SaveScreenshot('/home/wetzels/barycenter_four_clusters_pmd.png', renderView1, ImageResolution=[3006, 1586],
+    TransparentBackground=0)
+
+
+if __name__ == '__main__':
+    # generate extracts
+    SaveExtracts(ExtractsOutputDirectory='extracts')
---
+++ scripts/compute_matrices_four_clusters.py
@@ -0,0 +1,127 @@
+# state file generated using paraview version 5.10.1
+
+# uncomment the following three lines to ensure this script works in future versions
+#import paraview
+#paraview.compatibility.major = 5
+#paraview.compatibility.minor = 10
+
+import numpy as np
+from sklearn.manifold import TSNE
+from sklearn.manifold import MDS
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy.spatial as sp, scipy.cluster.hierarchy as hc
+
+#### import the simple module from the paraview
+from paraview.simple import *
+#### disable automatic camera reset on 'Show'
+paraview.simple._DisableFirstRenderCameraReset()
+
+# ----------------------------------------------------------------
+# setup the data processing pipelines
+# ----------------------------------------------------------------
+
+# create a new 'TTK CinemaReader'
+tTKCinemaReader1 = TTKCinemaReader(registrationName='TTKCinemaReader1', DatabasePath='vertical-swap-four-clusters.cdb')
+
+# create a new 'TTK CinemaProductReader'
+tTKCinemaProductReader1 = TTKCinemaProductReader(registrationName='TTKCinemaProductReader1', Input=tTKCinemaReader1)
+
+# create a new 'TTK MergeTree'
+tTKMergeTree1 = TTKMergeTree(registrationName='TTKMergeTree1', Input=tTKCinemaProductReader1)
+tTKMergeTree1.ScalarField = ['POINTS', 'scalar']
+tTKMergeTree1.InputOffsetField = ['POINTS', 'scalar']
+tTKMergeTree1.TreeType = 'Split Tree'
+
+# find source
+tTKMergeTree1_1 = FindSource('TTKMergeTree1')
+
+# create a new 'TTK BlockAggregator'
+tTKBlockAggregator1 = TTKBlockAggregator(registrationName='TTKBlockAggregator1', Input=[tTKMergeTree1, OutputPort(tTKMergeTree1_1,1)])
+tTKBlockAggregator1.FlattenInput = 0
+
+# create a new 'TTK MergeTreeDistanceMatrix'
+tTKMergeTreeDistanceMatrix1 = TTKMergeTreeDistanceMatrix(registrationName='TTKMergeTreeDistanceMatrix1', Input=tTKBlockAggregator1,
+    OptionalInput=None)
+tTKMergeTreeDistanceMatrix1.Backend = 'Path Mapping Distance (TopoInVis 2022)'
+tTKMergeTreeDistanceMatrix1.DistanceSquareRoot = 0
+tTKMergeTreeDistanceMatrix1.Epsilon1 = 0.0
+tTKMergeTreeDistanceMatrix1.Epsilon2 = 100.0
+tTKMergeTreeDistanceMatrix1.Epsilon3 = 100.0
+
+# set active source
+SetActiveSource(tTKMergeTreeDistanceMatrix1)
+
+tTKMergeTreeDistanceMatrix1.PathMappingLookahead = 0
+
+# save data
+SaveData('vertical-swap-four-clusters.cdb/dm_pmd.csv', proxy=tTKMergeTreeDistanceMatrix1, RowDataArrays=['Tree00', 'Tree01', 'Tree02', 'Tree03', 'Tree04', 'Tree05', 'Tree06', 'Tree07', 'Tree08', 'Tree09', 'Tree10', 'Tree11', 'Tree12', 'Tree13', 'Tree14', 'Tree15', 'Tree16', 'Tree17', 'Tree18', 'Tree19', 'treeID'],
+    FieldAssociation='Row Data')
+
+tTKMergeTreeDistanceMatrix1.Backend = 'Branch Mapping Distance (EuroVis 2022)'
+
+# save data
+SaveData('vertical-swap-four-clusters.cdb/dm_bmd.csv', proxy=tTKMergeTreeDistanceMatrix1, RowDataArrays=['Tree00', 'Tree01', 'Tree02', 'Tree03', 'Tree04', 'Tree05', 'Tree06', 'Tree07', 'Tree08', 'Tree09', 'Tree10', 'Tree11', 'Tree12', 'Tree13', 'Tree14', 'Tree15', 'Tree16', 'Tree17', 'Tree18', 'Tree19', 'treeID'],
+    FieldAssociation='Row Data')
+
+tTKMergeTreeDistanceMatrix1.Backend = 'Wasserstein Distance (IEEE TVCG 2021)'
+tTKMergeTreeDistanceMatrix1.DistanceSquareRoot = 1
+
+# save data
+SaveData('vertical-swap-four-clusters.cdb/dm_wsd.csv', proxy=tTKMergeTreeDistanceMatrix1, RowDataArrays=['Tree00', 'Tree01', 'Tree02', 'Tree03', 'Tree04', 'Tree05', 'Tree06', 'Tree07', 'Tree08', 'Tree09', 'Tree10', 'Tree11', 'Tree12', 'Tree13', 'Tree14', 'Tree15', 'Tree16', 'Tree17', 'Tree18', 'Tree19', 'treeID'],
+    FieldAssociation='Row Data')
+
+tTKMergeTreeDistanceMatrix1.Backend = 'Edit Distance (IEEE TVCG 2019)'
+tTKMergeTreeDistanceMatrix1.DistanceSquareRoot = 1
+
+# save data
+SaveData('vertical-swap-four-clusters.cdb/dm_mted.csv', proxy=tTKMergeTreeDistanceMatrix1, RowDataArrays=['Tree00', 'Tree01', 'Tree02', 'Tree03', 'Tree04', 'Tree05', 'Tree06', 'Tree07', 'Tree08', 'Tree09', 'Tree10', 'Tree11', 'Tree12', 'Tree13', 'Tree14', 'Tree15', 'Tree16', 'Tree17', 'Tree18', 'Tree19', 'treeID'],
+    FieldAssociation='Row Data')
+    
+   
+matrix0 = np.loadtxt(open("vertical-swap-four-clusters.cdb/dm_pmd.csv", "rb"), delimiter=",", skiprows=1,usecols=range(0,20))
+matrix1 = np.loadtxt(open("vertical-swap-four-clusters.cdb/dm_bmd.csv", "rb"), delimiter=",", skiprows=1,usecols=range(0,20))
+matrix2 = np.loadtxt(open("vertical-swap-four-clusters.cdb/dm_wsd.csv", "rb"), delimiter=",", skiprows=1,usecols=range(0,20))
+matrix3 = np.loadtxt(open("vertical-swap-four-clustersr.cdb/dm_mted.csv", "rb"), delimiter=",", skiprows=1,usecols=range(0,20))
+
+linkage = hc.linkage(sp.distance.squareform(matrix0), method='average')
+cm = sns.clustermap(matrix0, row_linkage=linkage, col_linkage=linkage, cmap='inferno_r')
+reorder = cm.dendrogram_row.reordered_ind
+fig, ax = plt.subplots()
+ax.imshow(matrix0[:,reorder][reorder], cmap='coolwarm', interpolation='nearest')
+ax.set_xticks([])
+ax.set_yticks([])
+fig.savefig("vertical-swap-four-clusters.cdb/dm_pmd.pdf", transparent=True, bbox_inches="tight", pad_inches=0.1)
+
+linkage = hc.linkage(sp.distance.squareform(matrix1), method='average')
+cm = sns.clustermap(matrix1, row_linkage=linkage, col_linkage=linkage, cmap='inferno_r')
+reorder = cm.dendrogram_row.reordered_ind
+fig, ax = plt.subplots()
+ax.imshow(matrix1[:,reorder][reorder], cmap='coolwarm', interpolation='nearest')
+ax.set_xticks([])
+ax.set_yticks([])
+fig.savefig("vertical-swap-four-clusters.cdb/dm_bmd.pdf", transparent=True, bbox_inches="tight", pad_inches=0.1)
+
+linkage = hc.linkage(sp.distance.squareform(matrix2), method='average')
+cm = sns.clustermap(matrix2, row_linkage=linkage, col_linkage=linkage, cmap='inferno_r')
+reorder = cm.dendrogram_row.reordered_ind
+fig, ax = plt.subplots()
+ax.imshow(matrix2[:,reorder][reorder], cmap='coolwarm', interpolation='nearest')
+ax.set_xticks([])
+ax.set_yticks([])
+fig.savefig("vertical-swap-four-clusters.cdb/dm_wsd.pdf", transparent=True, bbox_inches="tight", pad_inches=0.1)
+
+linkage = hc.linkage(sp.distance.squareform(matrix3), method='average')
+cm = sns.clustermap(matrix3, row_linkage=linkage, col_linkage=linkage, cmap='inferno_r')
+reorder = cm.dendrogram_row.reordered_ind
+fig, ax = plt.subplots()
+ax.imshow(matrix3[:,reorder][reorder], cmap='coolwarm', interpolation='nearest')
+ax.set_xticks([])
+ax.set_yticks([])
+fig.savefig("vertical-swap-four-clusters.cdb/dm_mted.pdf", transparent=True, bbox_inches="tight", pad_inches=0.1)
+ 
+
+
+if __name__ == '__main__':
+    # generate extracts
+    SaveExtracts(ExtractsOutputDirectory='extracts')
